9ab6f18cfae865446b3ad300560b2ecef95f89a6
Hadi Azaddel <m.h.azaddel@gmail.com>
2024-03-27 14:15:19 +0330

    Correctly close INSTALLED_APPS list in settings.py

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -5,12 +5,18 @@ import os
 SECRET_KEY = 'your-secret-key-here'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-LOOGADO_REDERECT_URL = 'console'
+LOGGER_REDIRECT_URL = 'console'
+
+# Added settings for installed apps, middleware, and databases
+# To keep things simple, we are using a simple setting for now
 INSTALLED_APPS = [
     'django.contribt.admin',
-    'project', # Your app 
-db = {
-    'DEFAULT': {
+    'project',
+ ]
+MIDDLEWARE = []
+ROOT_URLCONF = 'django_hello_world.urls'
+DATABASE_SETTINGS = {
+    'default': {
         'ENGINE': 'django.db.backends/sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
@@ -19,3 +25,4 @@ db = {
         'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3'),
     }
 }
+TZE_INFO = 'UTC'
